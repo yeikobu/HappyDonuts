1594384de29e39bd770f243dd53e7ed269de5412
Jacob Aguilar <aguilar.campos.jacob@gmail.com>
2022-08-01 19:26:02 -0400

    Custom tab view has been created

--- Happy Donuts/Views/Home/DashboardView.swift
+++ Happy Donuts/Views/Home/DashboardView.swift
@@ -7,14 +7,113 @@
 
 import SwiftUI
 
+let tabs = ["person.fill", "bag.fill", "heart.fill", "cart.fill"]
+let tabColors = ["green", "pink", "purple", "blue"]
+
 struct DashboardView: View {
+    
+    @State var backToLogin = false
+    @State var selectedTab = "bag.fill"
+    
     var body: some View {
-        Text("Dashboard")
+        
+        ZStack {
+            CustomTabView()
+                .navigationBarHidden(true)
+                .navigationBarBackButtonHidden(true)
+            
+        }
+        .background(Color("background"))
+        .ignoresSafeArea()
+    }
+    
+}
+
+struct TabButtonview: View {
+    
+    @Binding var selectedTab: String
+    var imageName: String
+    
+    var body: some View {
+        Button {
+            selectedTab = imageName
+            
+        } label: {
+            VStack {
+                Image(systemName: imageName)
+                    .foregroundColor(
+                        selectedTab == imageName && selectedTab == tabs[0] ? Color(tabColors[0]) :
+                        selectedTab == imageName && selectedTab == tabs[1] ? Color(tabColors[1]) :
+                        selectedTab == imageName && selectedTab == tabs[2] ? Color(tabColors[2]) :
+                        selectedTab == imageName && selectedTab == tabs[3] ? Color(tabColors[3]) :
+                        Color(.gray)
+                    )
+                    .font(.system(size: 32, weight: .bold))
+                    .padding(.bottom, 4)
+            }
+        }
+        
     }
 }
+
+
+struct CustomTabView: View {
+    
+    @State var selectedTab: String = "bag.fill"
+    @State var edge: CGFloat = 0.0
+    
+    var body: some View {
+        
+        ZStack(alignment: Alignment(horizontal: .center, vertical: .bottom)) {
+            
+            TabView(selection: $selectedTab) {
+                
+                Text("Profile")
+                    .tag("person.fill")
+                
+                Text("Home")
+                    .tag("bag.fill")
+                
+                Text("Liked")
+                    .tag("heart.fill")
+                
+                Text("Cart")
+                    .tag("cart.fill")
+            }
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+            .ignoresSafeArea(.all, edges: .bottom)
+            
+            HStack(spacing: 0) {
+                
+                ForEach(tabs, id: \.self) { imageName in
+                    TabButtonview(selectedTab: $selectedTab, imageName: imageName)
+                    if imageName != tabs.last {
+                        Spacer(minLength: 0)
+                    }
+                }
+                
+            }
+            .padding(.horizontal, 34)
+            .padding(.vertical, 26)
+            .background(
+                RoundedRectangle(cornerRadius: 25, style: .continuous)
+                    .foregroundColor(Color("background"))
+                    .shadow(color: Color("shadow"), radius: 2, x: 2, y: -2)
+            )
+            
+        }
+        
+        
+        
+        
+    }
+    
+}
+
 
 struct DashboardView_Previews: PreviewProvider {
     static var previews: some View {
         DashboardView()
     }
 }
+
